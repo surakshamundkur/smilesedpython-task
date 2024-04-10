2f0d2317864d3707b29b746378e738169c7e011b
surakshamundkur <154885639+surakshamundkur@users.noreply.github.com>
2024-04-10 09:04:28 +0000

    update

--- day02/Scenario1.py
+++ day02/Scenario1.py
@@ -6,15 +6,14 @@ print("number of apples=",count_apples )
 
 #2.	Extract the dairy items (milk and eggs):(including quantity)
 dairy_items = [item for item in shopping_list if item == "milk" or item == "eggs"]
-count_milk = selected_items.count("milk")
-count_eggs = selected_items.count("eggs")
-print("Selected items:", selected_items)  
+count_milk = dairy_items.count("milk")
+count_eggs = dairy_items.count("eggs")
+print("dairy_items:", dairy_items)  
 print("Count of 'milk':", count_milk)     
 print("Count of 'eggs':", count_eggs)    
 
 #3.	Create a new list with just the fruit names:
 fruits = ["apples", "bananas"]
-fruit_list = [item for item in shopping_list if item in fruits]
-print("Fruit List:", fruit_list)
+print(fruits)
 
 
